02aa95ccc2cf92ea37daf250754f11295ca6fdb1
John Hörnvall <john.minecraft@hotmail.se>
2017-04-21 22:30:04 +0200

    Next stage

--- Planet Game/Planet Game/Form1.cs
+++ Planet Game/Planet Game/Form1.cs
@@ -20,6 +20,8 @@ namespace Planet_Game
         private void Form1_Load(object sender, EventArgs e)
         {
             Console.WriteLine("Hello Earth");
+
+            Console.WriteLine("Hello World too");
         }
     }
 }
